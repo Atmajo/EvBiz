1234ab5015c89f43e39bcf431d428ccc1b00eb96
Atmajo <atmajoc@gmail.com>
2024-08-20 01:34:33 +0530

    chore: Update layout.tsx, package.json, page.tsx, update-card.css, update-form.tsx, and verfiy\page.tsx

--- src/app/(root)/(routes)/profile/update/page.tsx
+++ src/app/(root)/(routes)/profile/update/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import React from "react";
+import ProfileTab from "@/components/profile-tab";
+import UpdateCard from "@/components/update-card";
+import { useUser } from "@clerk/nextjs";
 
 const UpdatePage = () => {
+  const { user } = useUser();
   return (
     <section className="flex flex-col justify-center items-center px-4 py-10 h-full">
-      <h1>Update</h1>
+      <div className="flex mt-20 mb-44">
+        <UpdateCard
+          name={user?.fullName!}
+          img={user?.imageUrl!}
+          email={user?.emailAddresses[0].emailAddress!}
+        />
+      </div>
+      <ProfileTab />
     </section>
   );
 };
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Navbar from "@/components/navar";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="">
+    <main className="" suppressHydrationWarning>
       <Navbar />
       {children}
     </main>
